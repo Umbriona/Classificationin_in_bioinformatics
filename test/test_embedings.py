a9277e69d759c3cf0894b77e2ae3c0994fc7ee9d
sandra <33811221+Umbriona@users.noreply.github.com>
2023-06-11 19:27:51 +0200

    adding test for make_embeddings

--- test/test_embedings.py
+++ test/test_embedings.py
@@ -3,36 +3,50 @@ path_utils = os.path.dirname(os.getcwd())
 sys.path.append(path_utils)
 import unittest
 import pandas as pd
-from src.make_embedings import read_fasta
+from src.make_embedings import read_fasta, creat_embedings
 
 
 # TEST of test1.fasta
 TEST_FASTA_1 = "test1.fasta"
-TEST_FASTA_2 = "test1.fasta"
-TEST_FASTA_ROWS = 2
-TEST_FASTA_COLUMNS = 3
-TEST_FASTA_SEQUENCE_0 = "SANDRA"
-TEST_FASTA_ID_1 = "sequence1"
-TEST_FASTA_TM_1 = 90.0
+TEST_FASTA_2 = "test2.fasta"
+TEST_FASTA_1_ROWS = 2
+TEST_FASTA_1_COLUMNS = 3
+TEST_FASTA_1_SEQUENCE_0 = "SANDRA"
+TEST_FASTA_1_ID_1 = "sequence1"
+TEST_FASTA_1_TM_1 = 90.0
 
 
 
+# Testing Embeding
 
+TEST_FASTA_3 = "test3.fasta"
+TEST_FASTA_3_ROWS = 2
+TEST_FASTA_3_COLUMNS = 4
+TEST_FASTA_3_EMBEDDING_SIZE = 1024
 
 
 class TestReadFasta(unittest.TestCase):
     def test_shape(self): 
         df_fasta = read_fasta(TEST_FASTA_1)
-        self.assertEqual(TEST_FASTA_ROWS, df_fasta.shape[0])
-        self.assertEqual(TEST_FASTA_COLUMNS, df_fasta.shape[1])
+        self.assertEqual(TEST_FASTA_1_ROWS, len(df_fasta["seq"]))
+        self.assertEqual(TEST_FASTA_1_COLUMNS, len(df_fasta.keys()))
         
     def test_read(self):
         df_fasta = read_fasta(TEST_FASTA_1)
-        self.assertEqual(TEST_FASTA_SEQUENCE_0, df_fasta["sequence"][0].values)
-        self.assertEqual(TEST_FASTA_ID_1, df_fasta["id"][1].values)
-        self.assertEqual(TEST_FASTA_TM_1, df_fasta["ogt"][1].values)
+        self.assertEqual(TEST_FASTA_1_SEQUENCE_0, df_fasta["seq"][0])
+        self.assertEqual(TEST_FASTA_1_ID_1, df_fasta["id"][0])
+        self.assertEqual(TEST_FASTA_1_TM_1, df_fasta["TM"][1])
         
     def test_ids(self):
         self.assertRaises(ValueError, read_fasta, TEST_FASTA_2)
 
+
+class TestCreateEmbeding(unittest.TestCase):
+    def test_embeding(self): 
+        df_fasta = read_fasta(TEST_FASTA_3)
+        df_fasta = creat_embedings(df_fasta)
+        self.assertEqual(TEST_FASTA_3_ROWS, len(df_fasta["Embedding"]))
+        self.assertEqual(TEST_FASTA_3_COLUMNS, len(df_fasta.keys()))
+        self.assertEqual(TEST_FASTA_3_EMBEDDING_SIZE, df_fasta["Embedding"][0].shape[0])
         
+    
